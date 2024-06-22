a5206b3249bd90059f2b4f4b3ec52f65bf3a6db2
Flash3r01 <44031456+Flash3r01@users.noreply.github.com>
2024-06-22 11:17:36 +0200

    add predefined tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,30 @@
-import { z, defineCollection } from "astro:content"
+import { z, defineCollection, reference } from "astro:content"
 import { maxRating, minRating } from "../contants"
 
 const toolCollection = defineCollection({
   type: "data",
   schema: z.object({
     name: z.string(),
-    // TODO: Tags should be predefined, not arbitrary.
-    tags: z.array(z.string()),
+    tags: z.array(reference("tag")),
     rating: z.number()
       .min(minRating, `Rating must be higher or equal to ${minRating}`)
       .max(maxRating, `Rating must be less than or equal to ${maxRating}`),
     links: z.array(z.array(z.string()).length(2, "Links must be an array of display name and URL")),
-    creationDate: z.date(),
+    created: z.date(),
+  }),
+})
+
+const tagCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    created: z.date(),
+    color: z.string().regex(/^#[0-9a-f]{6}$/i, "Color must be a hexadecimal color code"),
   }),
 })
 
 export const collections = {
   tool: toolCollection,
+  tag: tagCollection,
 }
